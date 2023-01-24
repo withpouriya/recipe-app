5ee9a5fa9344bf99687fd8d55a316ce0f48a3bf0
withpouriya <pouriya.raj@gmail.com>
2023-01-24 06:36:50 +0330

    feat(recipes/recipe-details): add To Shopping List button functionlality

--- src/app/components/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,9 +10,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe!: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
   }
 
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -2,6 +2,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from '../components/recipes/recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Injectable({
   providedIn: 'root'
@@ -18,11 +19,14 @@ export class RecipeService {
   ];
   recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
   }
 
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
 
 }
